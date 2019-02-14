7bb440665971f0789cb535c3b9ca62eaaf59c10c
AprilLastCherry <397996067@qq.com>
2019-02-14 13:11:47 +0800

    router init

---
+++ vueRouter/src/router/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import home from '../home.vue';
+import about from '../about.vue';
+
+Vue.use(VueRouter);
+
+const routes=[
+    {
+        path:"/home",
+        component:home
+    },
+    {
+        path:'/about',
+        component:about
+    },
+    {
+        path:'/',
+        redirect:'/home'
+    }
+]
+
+var router = new VueRouter({
+    routes
+})
+
+export default router;
